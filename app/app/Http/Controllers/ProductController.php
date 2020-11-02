5cbdec28d86801d14552bfb320e8c7819e2edecb
ilyass jaatit <ilyassjaatit@gmail.com>
2020-11-02 17:30:06 +0100

    create StoreProductRequest and UpdateProductRequest

--- app/app/Http/Controllers/ProductController.php
+++ app/app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductCollection;
 use App\Models\Product;
@@ -25,7 +27,7 @@ class ProductController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
         return Product::create($request->all());
 
@@ -49,7 +51,7 @@ class ProductController extends Controller
      * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(UpdateProductRequest $request, Product $product)
     {
         return $product->update($request->all());
     }
